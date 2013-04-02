142d4bd488b5fe5fc9da9d6f3632bdb250c0717c
phbaa <phbaa@hotmail.com>
2013-04-02 18:25:59 -0300

    Create index.windows.php
    Página web referente aos comandos de interfaces de rede no windows.

---
+++ index.windows.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>IPP Networking Interfaces Project</title>
+  <link rel="stylesheet" href="background.css">
+
+</head>
+<body>
+<div id="div1"></div>
+<div id="div2"></div>
+
+<div id='cssmenu'>
+      <ul>
+         <li class='active'><a href='ping.php'><span>Ping</span></a></li>
+         <li><a href='tracert.php'><span>Tracert</span></a></li>
+         <li><a href='netstat.php'><span>Netstat</span></a></li>
+         <li><a href='ipconfig.php'><span>Ipconfig</span></a></li>
+         <li><a href='route.php'><span>Route</span></a></li>
+         <li class='last'><a href='about.php'><span>About</span></a></li>
+      </ul>
+    </div>
+
+
+
+</body>
+
+</html>
